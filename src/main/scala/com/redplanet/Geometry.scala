2541d15df16c90a65664fd93543dc28817294bbf
Cereopsis <Cereopsis@users.noreply.github.com>
2019-12-06 08:28:19 +0000

    Add notes re: refactorings

--- src/main/scala/com/redplanet/Geometry.scala
+++ src/main/scala/com/redplanet/Geometry.scala
@@ -64,3 +64,32 @@ case object Forward extends Direction { val vector = Point(1, 0) }
 case object CCW extends Direction { val vector = Point(0, 1) }
 case object CW extends Direction  { val vector = Point(0, -1) }
 case object Backward extends Direction { val vector = Point(-1, 0) }
+
+
+/*
+ * Ultimately, I think we need to present the following kind of interface
+ * to the user and all the above then falls within the purview of an interpreter.
+ * This would enable generating different kinds of output - e.g SVG
+ *
+ *   sealed trait Navigation[-A, +B] { self =>
+ *   
+ *     def >>>[C](that: Navigation[A, B]): Navigation[A, C] =
+ *       Navigation.Compose(self, that)
+ *   
+ *     def map[C](f: B => C): Navigation[A, C] = ???
+ *
+ *     ...
+ *   
+ *   }
+ *   
+ *   
+ *   object Navigation {
+ *     final case class Identity[A]() extends Navigation[A,A]
+ *     final case class Move(units: Int) extends Navigation[Point,Point]
+ *     final case class Rotate(direction: Direction) extends Navigation[Point,Point]
+ *     final case class Compose[A,B,C](left: Navigation[A,B], right: Navigation[B,C]) extends Navigation[A,C]
+ *   }
+ *   
+ *   // e.g
+ *   val graph = Move(5) >>> Rotate(CCW) >>> Move(8)
+ */
